07b7164b087ebe520f6f0b6ad0e72241eab2ed56
Moonyll <s.pouwel@laposte.net>
2018-12-18 13:50:56 +0100

    Cex2

---
+++ Cex2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bonjour, quel est votre âge ?");
+            int age = int.Parse(Console.ReadLine());
+            if (age >= 18)
+                Console.WriteLine("Vous avez " + age + " ans, vous êtes majeur.");
+            else
+                Console.WriteLine("Vous avez " + age + " ans, vous êtes mineur.");
+
+        }
+    }
+}
