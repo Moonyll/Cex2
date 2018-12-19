c696e1826f7d1dcdecbcdd4d88567786bc4dbda0
Moonyll <45941071+Moonyll@users.noreply.github.com>
2018-12-19 13:50:08 +0100

    Update Cex2.cs

--- Cex2.cs
+++ Cex2.cs
@@ -12,10 +12,18 @@ namespace ConsoleApp30
         {
             Console.WriteLine("Bonjour, quel est votre âge ?");
             int age = int.Parse(Console.ReadLine());
-            if (age >= 18)
-                Console.WriteLine("Vous avez " + age + " ans, vous êtes majeur.");
-            else
+            if (age >= 18 && age <=120)
+            {
+                Console.WriteLine("Vous avez " + age + " ans, vous êtes majeur."); //$ {age} "Vous avez
+            }
+            else if (age < 18 && >0) // else if == sinon si...
+            {
                 Console.WriteLine("Vous avez " + age + " ans, vous êtes mineur.");
+            }
+            else
+            {
+                Console.WriteLine("Oops, il doit y avoir une erreur");
+            }   
 
         }
     }
